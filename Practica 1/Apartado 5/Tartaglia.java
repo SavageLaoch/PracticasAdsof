3b19a17e3d9ba96b6319bddc52b936c94023dc3b
Juan Velasco <juveiz@localhost.localdomain>
2018-02-10 19:20:45 +0100

    Somos gilipoyas poero esta solucionado

--- Practica 1/Apartado 5/Tartaglia.java
+++ Practica 1/Apartado 5/Tartaglia.java
@@ -4,15 +4,15 @@
 * combinatoria.
 * @author Juan Velasco y Miguel Angel Sanchez.
 **/
-import java.util.*;
+
 
 public class Tartaglia {
 
   private Combinatoria c;
   private int n;
-  private Map<Integer, Long> cache = new HashMap<>();
 
-  /** 
+
+  /**
   * Construye un nuevo objeto de tipo Tartaglia con los parametros.
   * @param c Objeto combinatoria
   * @param n Numero de filas.
@@ -22,34 +22,24 @@ public class Tartaglia {
     this.n = n;
   }
 
-  private int posicion(int n, int k){ 
-    return n*(n+1)/2 + k;
-  }
 
-  /** 
+
+  /**
   * Convierte a texto el triangulo de Tartaglia.
   * @return Cadena de caracteres con el triangulo.
   **/
   public String toString(){
-    Long l;
     String s="";
     int i,j;
     for (i=0; i<n; i++){
       for (j=0; j<=i;j++){
-        if (cache.containsKey(posicion(i,j))==false) {
-          l = c.combinaciones(i,j);
-          s = s + " " + l;
-          cache.put(posicion(i,j),l);
-        }
-        else{
-          s = s + " " + cache.get(posicion(i,j));
-        }
+        s = s + " " + c.combinaciones(i,j);
       }
       s = s + "\n";
     }
     return s;
   }
-  /** 
+  /**
   * Punto de entrada a la aplicacion.
   * Este metodo imprime el triangulo de tartaglia asociado al
   * parametro de entrada
