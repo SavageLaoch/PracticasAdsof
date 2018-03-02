e2cb0e8424d3c0b5fde4fad8f81fa6164c6d6393
mikism98 <miguelangelsm98@hotmail.com>
2018-03-02 16:47:41 +0100

    genero y sesion

---
+++ Practica3/Sesion.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+/**
+ * @author Juan Velasco y Miguel Angel Sanchez.
+ *
+ */
+public class Sesion {
+
+  private Date fecha;
+  private Pelicula pelicula;
+  private int butacasDisponibles;
+  private int butacasTotales;  
+
+
+  public Sesion(Date fecha, Pelicula pelicula, int butacasDisponibles, int butacasTotales) {
+    this.fecha=fecha;
+    this.pelicula=pelicula;
+    this.butacasDisponibles=butacasDisponibles;
+    this.butacasTotales=butacasTotales;
+  }
+
+  public Date getFecha() {return fecha}
+
+  public Pelicula getPelicula() {return pelicula}
+
+  public int getButacasDisponibles() {return butacasDisponibles}
+
+  public int getButacasTotales() {return butacasTotales}
+
+  public void setButacasDisponibles(int butacasDisponibles) {
+    this.butacasDisponibles=butacasDisponibles;
+  }
+
+  public Boolean venderButacas(int butacasVendidas) {
+    int i;
+    i=this.butacasDisponibles-butacasVendidas;
+    if (i<0){
+      return false;
+    }else{
+      this.butacasDisponibles=i;
+      return true;
+    }
+  }
+
+
+
+  
