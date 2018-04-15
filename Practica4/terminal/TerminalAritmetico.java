f0c7fb4803d91bf10df23c8e54a2cb4dd454fc65
mikism98 <miguelangelsm98@hotmail.com>
2018-04-15 19:04:37 +0200

    javadocs terminal y terminalaritmetico

--- Practica4/terminal/TerminalAritmetico.java
+++ Practica4/terminal/TerminalAritmetico.java
@@ -1,12 +1,25 @@
 package terminal;
 import nodos.INodo;
 
+/**
+ * Clase terminal aritmetico
+ * @author Juan Velasco y Miguel Angel Sanchez
+ *
+ */
 public class TerminalAritmetico extends Terminal{
-
+	
+	/**
+	 * Crea un terminal aritmetico
+	 * @param raiz Raiz del terminal
+	 */
 	public TerminalAritmetico(String raiz) {
 		super(raiz);
 	}
 	
+	/**
+	 * Crea una copia un nodo del tipo TerminalAritmetico
+	 * @return INodo resultante de la copia
+	 */
 	public INodo copy() {
 		Terminal t = new TerminalAritmetico(this.getRaiz());
 		return t;
