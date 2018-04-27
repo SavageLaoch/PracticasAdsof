6fd2344d312d22dc35e437b1e07507962dfeaad5
SavageLaoch <juveiz2931@gmail.com>
2018-04-27 16:05:14 +0200

    wolo

---
+++ Practica5/src/vertice/Vertice.java
@@ -0,0 +1,19 @@
+package vertice;
+
+public class Vertice<T> {
+	 private final int id; // identificador del vértice dentro del grafo
+	 private T datos; // datos almacenados en el vértice
+	 
+	 public Vertice(int id1,T datos){
+		 this.id = id1;
+		 this.datos = datos;
+	 }
+
+	public int getId() {
+		return id;
+	}
+
+	public T getDatos() {
+		return datos;
+	}
+} 
