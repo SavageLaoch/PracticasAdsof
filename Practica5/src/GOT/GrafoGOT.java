0be12a697753bdba1b86f41610028282d3227b9d
mikism98 <miguelangelsm98@hotmail.com>
2018-05-04 14:01:06 +0200

    archivos

---
+++ Practica5/src/GOT/GrafoGOT.java
@@ -0,0 +1,36 @@
+package GOT;
+
+import java.util.*;
+import grafo.GrafoNoDirigido;
+import vertice.Vertice;
+
+public class GrafoGOT extends GrafoNoDirigido<PersonajeGOT>{
+	public GrafoGOT(String ficheroVertices, String ficheroArcos) throws Exception {
+			
+	}
+	
+	public Vertice<PersonajeGOT> getVertice(String nombre){
+		
+	}
+	
+	public List<String> casas(){
+		
+	}
+		
+	public List<String> miembrosCasa(String casa){
+		
+	}
+		
+	public Map<String, Integer> gradoPersonajes(){
+		
+	}
+			
+	public Map<String, Integer> gradoPonderadoPersonajes(){
+		
+	}
+				
+	public Map<String, Integer> personajesRelevantes(){
+		
+	}
+	
+}
---
+++ Practica5/src/GOT/PersonajeGOT.java
@@ -0,0 +1,29 @@
+package GOT;
+
+public class PersonajeGOT {
+	
+	private String nombre;
+	private String casa;
+	
+	public PersonajeGOT(String nombre, String casa) {
+		super();
+		this.nombre = nombre;
+		this.casa = casa;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getCasa() {
+		return casa;
+	}
+	
+	public void setCasa(String casa) {
+		this.casa = casa;
+	}
+}
