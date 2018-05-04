50c723a5c6a0e4caa65b7c24b79e7f421f158d09
mikism98 <miguelangelsm98@hotmail.com>
2018-05-04 14:53:08 +0200

    lambda

--- Practica5/src/GOT/GrafoGOT.java
+++ Practica5/src/GOT/GrafoGOT.java
@@ -4,6 +4,9 @@ import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.*;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
+
 import grafo.GrafoNoDirigido;
 import vertice.Vertice;
 
@@ -32,23 +35,52 @@ public class GrafoGOT extends GrafoNoDirigido<PersonajeGOT>{
 	}
 	
 	public Vertice<PersonajeGOT> getVertice(String nombre){
-		
+		return this.getVertices().stream().filter(v -> v.getDatos().getNombre().equals(nombre)).findFirst().get();
 	}
 	
 	public List<String> casas(){
-		
+		Set<String> set = this.getVertices().stream().map(Vertice<PersonajeGOT>::getDatos).map(PersonajeGOT::getCasa).collect(Collectors.toCollection(()->new TreeSet<String>()));
+		return new ArrayList<String>(set);
 	}
 		
 	public List<String> miembrosCasa(String casa){
+		Predicate<PersonajeGOT> predicado = new Predicate<PersonajeGOT>() {
+			@Override
+			public boolean test(PersonajeGOT personaje) {
+				return casa.equals(personaje.getCasa());
+			}
+		}
+		
+		List<String> personajes = new ArrayList<>();
+		
+		for(Vertice<PersonajeGOT> personaje: this.vertices.values()) {
+			if(predicado.test(personaje.getDatos())) {
+				personajes.add(personaje.getDatos().getNombre());
+			}
+		}
+		
+		return personajes;
 		
 	}
 		
 	public Map<String, Integer> gradoPersonajes(){
-		
+		Map<String,Integer> hash = new HashMap<>();
+		this.getVertices().stream().forEach((p) ->{
+			hash.put(p.getDatos().getNombre(),this.getVecinosDe(p).size());
+		});
+		return hash;
 	}
 			
-	public Map<String, Integer> gradoPonderadoPersonajes(){
-		
+	public Map<String, Double> gradoPonderadoPersonajes(){
+		Map<String,Double> hash = new HashMap<>();
+		this.getVertices().stream().forEach((p) ->{
+			double res = 0;
+			for(Vertice<PersonajeGOT> v: this.getVecinosDe(p)) {
+				res = res + this.getPesoDe(v, p);
+			}
+			hash.put(p.getDatos().getNombre(),res);
+		});
+		return hash;
 	}
 				
 	public Map<String, Integer> personajesRelevantes(){
